2ae7d99801ab1db6e33f86c58b626b9605fccef2
Ashton Fernandes <ashton@Ashtons-MacBook-Pro.local>
2018-04-03 01:56:57 -0400

    fixed completed task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import Home from './home/Home';
 import { Switch, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
     render() {
         return (
             <div className="body">
-                <main>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                    </Switch>
-                </main>
+                <MuiThemeProvider>
+                    <main>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                        </Switch>
+                    </main>
+                </MuiThemeProvider>
             </div>
         )
     }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,83 +1,166 @@
-import React, { Component } from 'react';
-import './Home.css';
+import React, { Component } from "react";
+import "./Home.css";
 import Header from "../header/Header";
+import DeleteIcon from "material-ui/svg-icons/action/delete";
+import Checkbox from "material-ui/Checkbox";
+import IconButton from "material-ui/IconButton";
+import Divider from "material-ui/Divider";
+import { ListItem } from "material-ui/List";
+import { blueGrey800, red600 } from "material-ui/styles/colors";
+import TextField from "material-ui/TextField";
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isHome: true,
-            isLogin: false,
-            task: "",
-            app: {
-                title: 'My tasks for the day',
-                options: ["study", "eat"]
-            },
-        };
-
-    }
-
-    onFormSubmit = (e) => {
-        e.preventDefault();
-        let newTask = this.state.app.options;
-        newTask.push(this.state.option);
-        this.setState({
-            options: newTask,
-            task: ""
-        });
-    }
-    
-    onMakeDecision = () => {
-        const randomNumber = Math.floor(Math.random() * this.state.app.options.length);
-        alert(this.state.app.options[randomNumber]);
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHome: true,
+      isLogin: false,
+      option: {
+          name: "",
+          taskCompleted: false
+      },
+      options : [
+        { name:'eat',taskCompleted: false },
+        { name:'sleep',taskCompleted: false }
+      ]
     };
-    
-    removeAll = (e) => {
-        this.setState({
-            app: {
-                options: []
-            }
-        });
-    };
+  }
 
-    handleInputChange = (e) => {
-        this.setState({
-            task: e.target.value
-        });
+  taskCompleted = (value) => {
+    const allOptions = this.state.options.map((option) => {
+        if(option.name === value.name){
+            option.taskCompleted =  !option.taskCompleted
+        } 
+        return option
+      });
+
+      this.setState({
+        options: allOptions,
+      });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.option.name !== "") {
+      let newTask = this.state.options;
+      newTask.push(this.state.option);
+      this.setState({
+        options: newTask,
+        option: {
+            name: ""
+        }
+      });
     }
+  };
 
-    render() {
-        return (
-            <div>
-                <div className="top-container">
-                    <Header
-                        isLogin={this.state.isLogin}
-                        isHome={this.state.isHome}
-                        isDashboard={this.state.isDashboard}
-                    />
-                    <div className="home-header">
-                        <h1>{this.state.app.title}</h1>
-                    </div>
-                </div>
+  onMakeDecision = () => {
+    const randomNumber = Math.floor(
+      Math.random() * this.state.options.length
+    );
+    alert(this.state.options[randomNumber]);
+  };
 
-                <div className="main-conatiner">
-                    <div className="button-container">
-                        <button className="button" disabled={this.state.app.options.length === 0} onClick={this.onMakeDecision}>What should I do?</button>
-                        <button className="button" onClick={this.removeAll}>Remove All</button>
-                    </div>
-                    <ol>
-                        {this.state.app.options.map((option) => <li key={option}>{option}</li>)}
-                    </ol>
-                    <form onSubmit={this.onFormSubmit}>
-                        <div className="submit-container" >
-                            <input className="input-field" type="text" name="task" value={this.state.task} onChange={this.handleInputChange}></input>
-                            <button className="button">Add task!</button>
-                        </div>
-                    </form>
+  removeAll = e => {
+    this.setState({
+      options: []
+    });
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      option: {
+          name: e.target.value
+      }
+    });
+  };
+
+  deleteTask = (option) => {
+    let newOptions = this.state.options.filter(function(item) {
+      return item !== option;
+    });
+    this.setState({
+      options: newOptions
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="top-container">
+          <Header
+            isLogin={this.state.isLogin}
+            isHome={this.state.isHome}
+            isDashboard={this.state.isDashboard}
+          />
+          <div className="home-header">
+            <h1>My tasks for the day</h1>
+          </div>
+        </div>
+
+        <div className="main-conatiner">
+          <div className="button-container">
+            <button
+              className="button"
+              disabled={this.state.options.length === 0}
+              onClick={this.onMakeDecision}
+            >
+              What should I do?
+            </button>
+            <button className="button" onClick={this.removeAll}>
+              Remove All
+            </button>
+          </div>
+          {this.state.options.map(option => (
+            <ListItem
+              key={option + "list"}
+              className="list-item"
+              rightIconButton={
+                <div className="item-buttons">
+                  <Checkbox
+                    className="list-checkbox"
+                    onCheck={() => this.taskCompleted(option)}
+                  />
+                  <IconButton
+                    tooltipPosition="bottom-right"
+                    iconStyle={{ color: red600 }}
+                    onClick={() => this.deleteTask(option)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
                 </div>
+              }
+            >
+  
+              {option.taskCompleted ? (
+                <div className="task-item-completed">
+                  <li style={{ textDecorationLine: 'line-through' }} key={option.name}>{option.name}</li>
+                </div>
+              ) : (
+                <div className="task-item-incomplete">
+                  <li key={option.name}>{option.name}</li>
+                </div>
+              )}
+              <Divider />
+            </ListItem>
+          ))}
+          <form onSubmit={this.onFormSubmit}>
+            <div className="submit-container">
+              <TextField
+                className="input-field"
+                fullWidth={true}
+                type="text"
+                name="option"
+                value={this.state.option.name}
+                onChange={this.handleInputChange}
+              />
+              <button className="button">Add task!</button>
             </div>
-        );
-    }
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
+
